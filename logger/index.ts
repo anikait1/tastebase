60146a11d8462f51f4953552a1ae079087e4636a
Anikait <anikait.makkar@outlook.com>
2025-10-01 13:48:02 +0530

    chore: remove unused imports

--- logger/index.ts
+++ logger/index.ts
@@ -1,4 +1,4 @@
-import pino, { type Bindings, type LoggerOptions } from "pino";
+import pino, { type LoggerOptions } from "pino";
 
 const level = Bun.env.LOG_LEVEL ?? "debug";
 const createBaseLogger = () => {
