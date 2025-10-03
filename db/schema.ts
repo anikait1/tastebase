461023a29afd60cfbc10b28bbb447d34da03d623
Anikait <anikait.makkar@outlook.com>
2025-10-03 10:23:13 +0530

    chore: remove recipe_jobs and related schema

--- db/schema.ts
+++ db/schema.ts
@@ -12,8 +12,11 @@ import type { Ingredient } from "../recipe/type";
 
 export const recipe_source_schema = pgTable("recipe_sources", {
   id: serial("id").primaryKey(),
-  external_id: text("source_id").notNull().unique(),
   type: text("type").notNull(),
+  external_id: text("external_id").notNull().unique(),
+  created_at: timestamp("created_at")
+    .notNull()
+    .default(sql`now()`),
   metadata: jsonb("metadata"),
 });
 
@@ -24,11 +27,14 @@ export const recipe_schema = pgTable("recipes", {
     .notNull(),
   name: text("name").notNull(),
   instructions: text("instructions").notNull(),
+  ingredients: jsonb("ingredients").$type<Ingredient[]>().notNull(),
   tags: text("tags")
     .array()
     .notNull()
     .default(sql`ARRAY[]::text[]`),
-  ingredients: jsonb("ingredients").$type<Ingredient[]>().notNull(),
+  created_at: timestamp("created_at")
+    .notNull()
+    .default(sql`now()`),
 });
 
 export const embedding_schema = pgTable("embeddings", {
@@ -40,42 +46,14 @@ export const embedding_schema = pgTable("embeddings", {
   data: vector({ dimensions: 1536 }),
 });
 
-export const recipe_job_schema = pgTable("recipe_jobs", {
+export const content_item_schema = pgTable("content_items", {
   id: serial("id").primaryKey(),
   recipe_source_id: integer("recipe_source_id")
     .references(() => recipe_source_schema.id)
-    .notNull()
-    .unique(),
-  status: text("status").notNull().default("created"),
+    .notNull(),
+  stage: text("stage").notNull(),
+  data: jsonb("data").notNull(),
   created_at: timestamp("created_at")
     .notNull()
     .default(sql`now()`),
-  started_at: timestamp("started_at"),
-  updated_at: timestamp("updated_at"),
-  completed_at: timestamp("completed_at"),
-  error_message: text("error_message"),
-});
-
-export const job_step_schema = pgTable("job_steps", {
-  id: serial("id").primaryKey(),
-  job_id: integer("job_id")
-    .references(() => recipe_job_schema.id)
-    .notNull(),
-  type: text("type").notNull(),
-  order: integer("order").notNull(),
-  status: text("status").notNull().default("created"),
-  error_message: text("error_message"),
-  started_at: timestamp("started_at"),
-  completed_at: timestamp("completed_at"),
-  updated_at: timestamp("updated_at"),
-  metadata: jsonb("metadata"),
-});
-
-export const content_item_schema = pgTable("content_items", {
-  id: serial("id").primaryKey(),
-  job_step_id: integer("job_step_id")
-    .references(() => job_step_schema.id)
-    .notNull(),
-  content: jsonb("content").notNull(),
-  metadata: jsonb("metadata"),
 });
